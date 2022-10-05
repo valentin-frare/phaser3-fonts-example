32e78f45e2db33ee4dae078998e9ad0a49196201
ValentinFrare <valentin.frare@sismogames.com>
2022-10-05 11:01:42 -0300

    uwu

--- src/scenes/HelloWorldScene.js
+++ src/scenes/HelloWorldScene.js
@@ -19,34 +19,10 @@ export default class HelloWorldScene extends Phaser.Scene
 
         console.log(FontsTool.loadFont('Fuentecita-ttf', 'assets/fonts/Joynoted Demo.ttf'));
         console.log(FontsTool.loadFont('Fuentecita-otf', 'assets/fonts/Gemini Moon.otf'));
-
-        this.load.setBaseURL('http://labs.phaser.io')
-
-        this.load.image('sky', 'assets/skies/space3.png')
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png')
-        this.load.image('red', 'assets/particles/red.png')
     }
 
     create()
     {
-        this.add.image(400, 300, 'sky')
-
-        const particles = this.add.particles('red')
-
-        const emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        })
-
-        const logo = this.physics.add.image(400, 100, 'logo')
-
-        logo.setVelocity(100, 200)
-        logo.setBounce(1, 1)
-        logo.setCollideWorldBounds(true)
-
-        emitter.startFollow(logo)
-
         this.add.text(400, 100, 'Wenas, fuente de Google', {
             fontFamily: 'Droid Sans',
             fontSize: '42px',
